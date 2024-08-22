663e55b72d531362e39bf66d7501927b69ffe145
Chan Myae Oo <chanmyaeoodev@gmail.com>
2024-08-22 19:53:04 +0630

    update

--- src/services/tableService.js
+++ src/services/tableService.js
@@ -1,32 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5001';
-
+const API_URL = "http://localhost:5001";
 
 export const fetchTableByNumber = async (tableNo) => {
+  try {
+    const response = await axios.get(`${API_URL}/tables`);
+    const tables = response.data;
+    const tableNoAsNumber = Number(tableNo);
 
-    try {
-        const response = await axios.get(`${API_URL}/tables`);
-        const tables = response.data;
-        const tableNoAsNumber = Number(tableNo);
+    const table = tables.find((t) => t.tableNo === tableNoAsNumber);
+    console.log("Found table:", table);
 
-        const table = tables.find(t => t.tableNo === tableNoAsNumber);
-        console.log("Found table:", table);
-
-        return table;
-    } catch (err) {
-        console.error('Error fetching table data:', err);
-        throw err;
-    }
+    return table;
+  } catch (err) {
+    console.error("Error fetching table data:", err);
+    throw err;
+  }
 };
-
 
 export const updateTableOrders = async (tableId, updatedOrders) => {
-    try {
-        await axios.patch(`${API_URL}/tables/${tableId}`, { orders: updatedOrders });
-    } catch (err) {
-        console.error('Error updating table orders:', err);
-        throw err;
-    }
+  try {
+    await axios.patch(`${API_URL}/tables/${tableId}`, {
+      orders: updatedOrders,
+    });
+  } catch (err) {
+    console.error("Error updating table orders:", err);
+    throw err;
+  }
 };
-
